0c4e2aec3720651fedfb8403240a6210fc0852b7
yeinshin <syi05111@naver.com>
2021-05-19 15:31:23 +0900

    Update programmers_카펫.py

--- programmers/programmers_카펫.py
+++ programmers/programmers_카펫.py
@@ -3,17 +3,16 @@
 
 def solution(brown, yellow):
     answer = []
-    
+
     for i in range(1,yellow+1):
         if yellow % i==0:
             height = i #세로길이
             width = yellow // i #가로길이
-            
+
             if ((height*2) + (width*2) + 4 )==brown:
                 answer.append(width+2)
                 answer.append(height+2)
-                
+
                 break
-                
-    
+            
     return answer
