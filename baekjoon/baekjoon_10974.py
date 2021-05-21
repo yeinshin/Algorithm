2b49da27e0dbb8bf9780e2ce47b0aa16adcfc108
yeinshin <syi05111@naver.com>
2021-05-21 16:49:16 +0900

    백준10974번-모든 순열

---
+++ baekjoon/baekjoon_10974.py
@@ -0,0 +1,12 @@
+#10974번-모든 순열
+
+from itertools import permutations
+
+n=int(input())
+l = list(range(1,n+1))
+l = sorted(list(permutations(l,n)))
+
+for i in l:
+    for j in range(n):
+        print(i[j],end=' ')
+    print()
