f4571a1cb35187d8d38df9894505211958eda6a8
yeinshin <syi05111@naver.com>
2021-05-25 21:30:57 +0900

    dfs로 만드는 조합

---
+++ algorithm/dfs_combination.py
@@ -0,0 +1,23 @@
+#dfs로 만드는 조합
+
+n,m = map(int,input().split())
+visited = [False]*(n+1)
+arr = list()
+
+def dfs(cnt):
+    if cnt==m:
+        print(*arr)
+        return
+    
+    for i in range(1,n+1):
+        if not visited[i]: #방문을 하지 않았을 때!
+            visited[i]=True
+            arr.append(i)
+            
+            dfs(cnt+1)
+            arr.pop()
+
+            for j in range(i+1,n): #i보다 큰 경우만 visited를 False으로 바꿔줌
+                visited[j]=False
+            
+dfs(0)
