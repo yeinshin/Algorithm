71adf11573bcab1d96194174567bb8ac6459271e
yeinshin <syi05111@naver.com>
2021-06-22 00:53:42 +0900

    백준1463번-1로 만들기

---
+++ baekjoon/baekjoon_1463.py
@@ -0,0 +1,18 @@
+#https://www.acmicpc.net/problem/1463
+#1463번-1로 만들기/풀이 참고함
+
+n = int(input())
+
+dp = [0]*(n+1)
+
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + 1  
+
+    if i%2 == 0 and dp[i] > dp[i//2] + 1 :
+        dp[i] = dp[i//2]+1
+        
+    if i%3 == 0 and dp[i] > dp[i//3] + 1 :
+        dp[i] = dp[i//3] + 1
+        
+print(dp[n])
+    
