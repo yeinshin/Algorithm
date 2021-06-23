8340c0f9678823d47ad64fe199b8a5e9d5cf7ae3
yeinshin <syi05111@naver.com>
2021-06-23 23:54:48 +0900

    백준1890번-점프

---
+++ baekjoon/baekjoon_1890.py
@@ -0,0 +1,18 @@
+#https://www.acmicpc.net/problem/1890
+#1890번-점프
+import sys
+input = sys.stdin.readline
+n = int(input())
+graph = [list(map(int,input().split())) for _ in range(n)]
+dp = [[0]*n for _ in range(n)]
+dp[0][0]=1
+
+for i in range(n):
+    for j in range(n):
+        if dp[i][j]!=0 and graph[i][j]!=0:
+            if i+graph[i][j]<n:
+                dp[i+graph[i][j]][j]+=dp[i][j]
+            if j+graph[i][j]<n:
+                dp[i][j+graph[i][j]]+=dp[i][j]
+
+print(dp[-1][-1])
