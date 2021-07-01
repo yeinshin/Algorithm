7755ae0a9f42f286e1430c573ef92a02ac038667
yeinshin <syi05111@naver.com>
2021-07-02 00:54:46 +0900

    visited 리스트 삭제

--- programmers/progrmmers_타겟넘버.py
+++ programmers/progrmmers_타겟넘버.py
@@ -1,7 +1,7 @@
 #https://programmers.co.kr/learn/courses/30/lessons/43165
 #프로그래머스-타겟 넘버
 answer = 0
-def dfs(result,cnt,numbers,target,visited):
+def dfs(result,cnt,numbers,target):
     global answer
     
     if cnt==len(numbers):
@@ -10,10 +10,9 @@ def dfs(result,cnt,numbers,target,visited):
         return
     
     for j in (-numbers[cnt],+numbers[cnt]):
-        dfs(result+j,cnt+1,numbers,target,visited)
+        dfs(result+j,cnt+1,numbers,target)
             
 def solution(numbers, target):
     global answer
-    visited = [False]*(len(numbers))
-    dfs(0,0,numbers,target,visited)
+    dfs(0,0,numbers,target)
     return answer
