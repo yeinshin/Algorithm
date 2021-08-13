043c7cf1b7cc2df56a01ba4819cdc15c1ca04660
yeinshin <syi05111@naver.com>
2021-08-13 20:29:18 +0900

    프로그래머스-H-Index

---
+++ programmers/programmers_H-Index.py
@@ -0,0 +1,10 @@
+#프로그래머스-H-Index
+def solution(citations):
+    answer = 0
+    citations.sort(reverse=True)
+    
+    for i in range(len(citations)):
+        if citations[i]>=(i+1):
+            answer+=1
+            
+    return answer
