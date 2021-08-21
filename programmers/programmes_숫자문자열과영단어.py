3ebd46dbb6c0d51b90934c634e436cf1bf1343ec
yeinshin <syi05111@naver.com>
2021-08-21 22:18:57 +0900

    다른 풀이 방법 추가

--- programmers/programmes_숫자문자열과영단어.py
+++ programmers/programmes_숫자문자열과영단어.py
@@ -29,3 +29,25 @@ def solution(s):
             cnt = 0
     
     return int(''.join(answer))
+
+    
+    # 또 다른 풀이
+
+    def solution(s):
+        words = {
+            'zero':'0',
+            'one':'1',
+            'two':'2',
+            'three':'3',
+            'four':'4',
+            'five':'5',
+            'six':'6',
+            'seven':'7',
+            'eight':'8',
+            'nine':'9'
+        }
+        
+        for word in words.items():
+            s=s.replace(word[0],word[1])
+        
+        return int(s)
