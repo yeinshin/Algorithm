7660d6b9715d2f682e85691678033c1a3f70e6e1
yeinshin <syi05111@naver.com>
2021-08-21 22:13:21 +0900

    프로그래머스-숫자 문자열과 영단어

---
+++ programmers/programmes_숫자문자열과영단어.py
@@ -0,0 +1,31 @@
+#https://programmers.co.kr/learn/courses/30/lessons/81301
+#프로그래머스-숫자 문자열과 영단어
+
+def solution(s):
+    answer = []
+    words = {
+        'zero':'0',
+        'one':'1',
+        'two':'2',
+        'three':'3',
+        'four':'4',
+        'five':'5',
+        'six':'6',
+        'seven':'7',
+        'eight':'8',
+        'nine':'9'
+    }
+    
+    cnt = 0
+    
+    for i in range(len(s)):
+        if s[i].isalpha():
+            cnt+=1
+            if cnt>=3 and s[i-cnt+1:i+1] in words:
+                answer.append(words[s[i-cnt+1:i+1]])
+                cnt = 0
+        else:
+            answer.append(s[i])
+            cnt = 0
+    
+    return int(''.join(answer))
