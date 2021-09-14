232d5afa008364f77cb3e014de15951a11806879
yeinshin <syi05111@naver.com>
2021-09-14 20:45:41 +0900

    프로그래머스-행렬의 덧셈

---
+++ programmers/programmers_행렬의덧셈.py
@@ -0,0 +1,8 @@
+#https://programmers.co.kr/learn/courses/30/lessons/12950
+#프로그래머스-행렬의 덧셈
+def solution(arr1, arr2):
+    answer = [[] for _ in range(len(arr1))]
+    for i in range(len(arr1)):
+        for j in range(len(arr1[0])):
+            answer[i].append(arr1[i][j]+arr2[i][j])
+    return answer
