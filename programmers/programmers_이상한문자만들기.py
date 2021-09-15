e50f913120595a2fb0c7c8f380ddfb92074b2122
yeinshin <syi05111@naver.com>
2021-09-16 00:00:30 +0900

    코드 수정

--- programmers/programmers_이상한문자만들기.py
+++ programmers/programmers_이상한문자만들기.py
@@ -1,13 +1,11 @@
 #https://programmers.co.kr/learn/courses/30/lessons/12930
 #프로그래머스-이상한 문자 만들기
-def solution(s): 
-    s_split = s.split(" ") 
-    for k in range(len(s_split)): 
-        s_list = list(s_split[k]) 
-        for i in range(len(s_list)): 
-            if i % 2 == 0: s_list[i] = s_list[i].upper() 
-            elif i % 2 == 1: 
-                s_list[i] = s_list[i].lower() 
-            s_split[k] = "".join(s_list) 
-            answer = " ".join(s_split) 
-    return answer
+def solution(s):
+    answer = list()
+    for i in s.split(' '):
+        c = ''
+        for j in range(len(i)):
+            if j%2==0:c+=i[j].upper()
+            else: c+=i[j].lower()
+        answer.append(c)
+    return ' '.join(answer)
