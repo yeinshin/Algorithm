675c3a95025981e92474f6b07a605e5a4b2dc847
yeinshin <syi05111@naver.com>
2021-09-15 23:58:50 +0900

    프로그래머스-이상한 문자 만들기

---
+++ programmers/programmers_이상한문자만들기.py
@@ -0,0 +1,13 @@
+#https://programmers.co.kr/learn/courses/30/lessons/12930
+#프로그래머스-이상한 문자 만들기
+def solution(s): 
+    s_split = s.split(" ") 
+    for k in range(len(s_split)): 
+        s_list = list(s_split[k]) 
+        for i in range(len(s_list)): 
+            if i % 2 == 0: s_list[i] = s_list[i].upper() 
+            elif i % 2 == 1: 
+                s_list[i] = s_list[i].lower() 
+            s_split[k] = "".join(s_list) 
+            answer = " ".join(s_split) 
+    return answer
