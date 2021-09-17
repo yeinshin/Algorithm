1515bf0611e9e3837b1b5a65eb2ef0e7acba1786
yeinshin <syi05111@naver.com>
2021-09-17 13:08:50 +0900

    백준2468번-키 순서

---
+++ baekjoon/baekjoon_2458.py
@@ -0,0 +1,20 @@
+#https://www.acmicpc.net/problem/2458
+#2468번-키 순서
+INF = int(1e9)
+n,m = map(int,input().split())
+graph = [[INF]*(n+1) for _ in range(n+1)]
+for _ in range(m):
+    a,b = map(int,input().split())
+    graph[a][b]=1
+for k in range(1,n+1):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            if graph[i][k]+graph[k][j]==2: graph[i][j]=1
+
+cnt = [0]*(n+1)
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if graph[i][j]==1:
+            cnt[i]+=1
+            cnt[j]+=1
+print(cnt.cnt(n-1))
