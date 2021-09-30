2fe58f02be109e19dd42c58a7498024641fc3b5f
yeinshin <syi05111@naver.com>
2021-09-30 17:05:22 +0900

    백준10815번-숫자 카드 2

---
+++ baekjoon/baekjoon_10815.py
@@ -0,0 +1,16 @@
+#https://www.acmicpc.net/problem/10815
+#10815번-숫자 카드 2
+n = int(input())
+card = sorted(map(int,input().split()))
+m = int(input())
+
+def binary_search(start,end,target):
+    while start<=end:
+        mid = (start+end)//2
+        if card[mid]==target: return 1
+        elif card[mid]<target:start = mid +1
+        else: end = mid -1
+    return 0
+
+for t in list(map(int,input().split())):
+    print(binary_search(0,n-1,t))
