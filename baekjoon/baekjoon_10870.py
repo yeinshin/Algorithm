488937fe2216e2b70df694e4123a26b671d1b064
yeinshin <syi05111@naver.com>
2021-10-05 17:25:08 +0900

    백준10870번-피보나치 수 5

---
+++ baekjoon/baekjoon_10870.py
@@ -0,0 +1,11 @@
+#https://www.acmicpc.net/problem/10870
+#10870번-피보나치 수 5
+a = [0]*21
+a[1]=1
+n = int(input())
+if 0<=n<=1: print(a[n])
+else:
+    for i in range(2,n+1):
+        a[i]=a[i-1],a[i-2]
+    print(a[n])
+
