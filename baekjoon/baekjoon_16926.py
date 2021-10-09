9ffa31f1baeb43f5fe14d90c699a8fbf5d0b89dd
yeinshin <syi05111@naver.com>
2021-10-10 00:40:35 +0900

    백준16926번-배열 돌리기1

--- baekjoon/baekjoon_16926.py
+++ baekjoon/baekjoon_16926.py
@@ -1,10 +1,34 @@
 #https://www.acmicpc.net/problem/16926
-#16926번-배열 돌리기1
+#16926번-배열 돌리기1(풀이참고)
 n,m,r = map(int,input().split())
 array = [list(map(int,input().split())) for _ in range(n)]
-cnt = 0
-while cnt<=r:
-    cnt+=1
-    if n>=3 and m>=3:
-        for x,y in ((0,0),(1,1)):
-            
+
+for _ in range(r):
+    for i in range(min(n,m)//2):
+        s_x,s_y = i,i
+        now = array[i][i]
+        for j in range(i+1,n-i):
+            s_x=j
+            temp = array[s_x][s_y]
+            array[s_x][s_y]=now
+            now = temp
+        for j in range(i+1,m-i):
+            s_y=j
+            temp = array[s_x][s_y]
+            array[s_x][s_y]=now
+            now = temp
+        for j in range(i+1,n-i):
+            s_x = n - j - 1
+            temp=array[s_x][s_y]
+            array[s_x][s_y]=now
+            now=temp
+        for j in range(i+1,m-i):
+            s_y = m - j - 1
+            temp=array[s_x][s_y]
+            array[s_x][s_y]=now
+            now=temp
+
+for i in range(n):
+    for j in range(n):
+        print(array[i][j],end=' ')
+    print()
