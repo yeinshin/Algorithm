83da45f4823684eba2f63972f68711b469a03213
yeinshin <syi05111@naver.com>
2021-11-13 02:28:54 +0900

    코드 수정

--- baekjoon/baekjoon_17142.py
+++ baekjoon/baekjoon_17142.py
@@ -5,30 +5,34 @@ from collections import deque
 n,m = map(int,input().split())
 graph = [list(map(int,input().split())) for _ in range(n)]
 virus = []
+walls = 0
 q = deque()
 for i in range(n):
     for j in range(n):
-        if virus[i][j]==2: virus.append((i,j))
+        if graph[i][j]==2: virus.append((i,j))
+        elif graph[i][j]==1: walls+=1
 
-def bfs(n):
+def bfs(n,walls):
     while q:
-        x,y = q.popleft()
+        x,y,d,b = q.popleft()
         for nx,ny in ((x+1,y),(x-1,y),(x,y+1),(x,y-1)):
-            if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
+            if 0<=nx<n and 0<=ny<n and visited[nx][ny]==-1:
                 if graph[nx][ny]==2:
-                    visited[nx][ny]=visited[x][y]
+                    visited[nx][ny]=0
                     check[nx][ny]=0
-                    q.append((nx,ny))
+                    q.append((nx,ny,d,b+1))
                 elif graph[nx][ny]==0:
-                    visited[nx][ny]=visited[x][y]+1
+                    if b!=0:
+                        visited[nx][ny]=d+b
+                    else:visited[nx][ny]=visited[x][y]+1
                     check[nx][ny]=0
-                    q.append((nx,ny))
+                    q.append((nx,ny,visited[nx][ny],0))
     cnt = 0
     sec = -1
     for i in range(n):
         cnt+=sum(check[i])
         sec = max(sec,max(visited[i]))
-    return sec if cnt==len(virus) else INF
+    return sec if cnt==walls else INF
 
 
 INF = int(1e9)
@@ -37,10 +41,10 @@ for comb in combinations(virus,m):
     visited = [[-1]*n for _ in range(n)]
     check = [[1]*n for _ in range(n)]
     for x,y in comb:
-        q.append((x,y))
+        q.append((x,y,0,0))
         visited[x][y]=0
         check[x][y]=0
-    ans=min(ans,bfs(n))
+    ans=min(ans,bfs(n,walls))
 
 print(ans if ans!=INF else -1)
 
