e644bef0078d5e196cebc261e0a910762b747b0b
yeinshin <syi05111@naver.com>
2021-11-18 17:56:11 +0900

    코드 수정

--- baekjoon/baekjoon_9207.py
+++ baekjoon/baekjoon_9207.py
@@ -1,41 +1,41 @@
 #https://www.acmicpc.net/problem/9207
 #9207번-페그 솔리테어
-from collections import deque
+import sys
+sys.setrecursionlimit(100000)
 t = int(input())
+dx,dy = [1,-1,0,0],[0,0,-1,1]
+
+def dfs(board,cnt):
+        global check
+        for x in range(5):
+            for y in range(9):
+                if board[x][y]=='o':
+                    for i in range(4):
+                        nx = dx[i]+x
+                        ny = dy[i]+y
+                        if 0<=nx<5 and 0<=ny<9 and board[nx][ny]=='o':
+                            nnx = dx[i]+nx
+                            nny = dy[i]+ny
+                            if 0<=nnx<5 and 0<=nny<9 and board[nnx][nny]=='.':
+                                board[x][y]='.'
+                                board[nx][ny]='.'
+                                board[nnx][nny]='o'
+                                dfs(board,cnt+1)
+                                board[x][y]='o'
+                                board[nx][ny]='o'
+                                board[nnx][nny]='.'                       
+        check = max(check,cnt)
+        return
+
 for i in range(t):
     board = [list(input()) for _ in range(5)]
     if i!=t-1: s=input()
-    pin = []
+    pin = 0
     ans = []
     for i in range(5):
         for j in range(9):
-            if board[i][j]=='o':pin.append((i,j))
-    
-    for a,b in pin:
-        print('pin a:',a,'pin b:',b)
-        q = deque([(a,b,0)])
-        visited = [[0]*9 for _ in range(5)]
-        visited[a][b]=1
-        check = 0
-        while q:
-            x,y,cnt = q.popleft()
-            print('x:',x,'y:',y,'cnt:',cnt)
-            check = max(check,cnt)
-            if 0<=x+2<5 and not visited[x+1][y] and not visited[x+2][y] and board[x+1][y]=='o' and board[x+2][y]=='.':
-                visited[x+1][y]=1
-                visited[x+2][y]=1
-                q.append((x+2,y,cnt+1))
-            if 0<=x-2<5 and not visited[x-1][y] and not visited[x-2][y] and board[x-1][y]=='o' and board[x-2][y]=='.':
-                visited[x-1][y]=1
-                visited[x-2][y]=1
-                q.append((x-2,y,cnt+1))
-            if 0<=y+2<9 and not visited[x][y+1] and not visited[x][y+2] and board[x][y+1]=='o' and board[x][y+2]=='.':
-                visited[x][y+1]=1
-                visited[x][y+2]=1
-                q.append((x,y+2,cnt+1))
-            if 0<=y-2<9 and not visited[x][y-1] and not visited[x][y-2] and board[x][y-1]=='o' and board[x][y-2]=='.':
-                visited[x][y-1]=1
-                visited[x][y-2]=1
-                q.append((x,y-2,cnt+1))
-        ans.append((check,len(pin)-check))
-    print(ans)
+            if board[i][j]=='o':pin+=1
+
+    check = 0
+    dfs(board,0)
+    print(pin-check,check)
