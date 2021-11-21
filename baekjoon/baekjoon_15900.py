8453d0f627b8dc1d4c73e467cebab20405b252e7
yeinshin <syi05111@naver.com>
2021-11-21 22:05:14 +0900

    코드 수정

--- baekjoon/baekjoon_15900.py
+++ baekjoon/baekjoon_15900.py
@@ -1,15 +1,32 @@
 #https://www.acmicpc.net/problem/15900
 #15900번-나무 탈출
-from collections import defaultdict
+import sys
+sys.setrecursionlimit(10**5)
+
 n = int(input())
+answer = 0
 graph = [[] for _ in range(n+1)]
-cnt = defaultdict(int)
+distance = [-1 for _ in range(n+1)]
+distance[1]=0
+
 for _ in range(n-1):
-    a,b = map(int,input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-    cnt[a]+=1
-    cnt[b]+=1
-visited = [False]*(n+1)
-visited[1]=True
-print(cnt)
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+def dfs(cur):
+    for next in graph[cur]:
+        if distance[next]==-1:
+            distance[next] = distance[cur] + 1 
+            dfs(next)
+
+dfs(1)
+
+for i in range(2,n+1):
+    if len(graph[i]) == 1:
+        answer += distance[i]
+
+if answer % 2 == 0:
+    print("No")
+else:
+    print("Yes")
