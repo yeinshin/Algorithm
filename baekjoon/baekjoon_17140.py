b7a44d4de7833172c1a8cda7e3021e09dbc22a11
yein <syi05111@naver.com>
2022-01-26 09:21:42 +0900

    변수 삭제, 코드 수정

--- baekjoon/baekjoon_17140.py
+++ baekjoon/baekjoon_17140.py
@@ -9,8 +9,6 @@ input = sys.stdin.readline
 r,c,k = map(int,input().split())
 A = [list(map(int,input().split())) for _ in range(3)]
 ans = 1
-n ,m = 3, 3
-
 def calc (A,a):
     max_len = 0
     for i in range(len(A)):
@@ -27,13 +25,13 @@ def calc (A,a):
 if r<=len(A) and c<=len(A) and A[r-1][c-1]==k: print(0)
 else:
     while ans<=100:
-        if n<m: # C 연산
-            a = [[] for _ in range(m)]
+        if len(A)<len(A[0]): # C 연산
+            a = [[] for _ in range(len(A[0]))]
             A = list(map(list, zip(*A)))
             calc(A,a)
             a = list(map(list,zip(*a)))
         else:   # R 연산
-            a = [[] for _ in range(n)]
+            a = [[] for _ in range(len(A))]
             calc(A,a)
 
         if r<=len(a) and c<=len(a[0]) and a[r-1][c-1]==k: 
@@ -41,6 +39,5 @@ else:
             break
         
         ans+=1
-        n ,m = len(a), len(a[0])
         A = deepcopy(a)
     else: print(-1)
