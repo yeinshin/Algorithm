ae0934f479a5c8a0dbed557bfbe7b7d93159379a
yein <syi05111@naver.com>
2022-01-28 21:19:48 +0900

    프로그래머스-괄호 회전하기(코드 리팩토링)

--- programmers/programmers_괄호회전하기.py
+++ programmers/programmers_괄호회전하기.py
@@ -1,34 +1,20 @@
 #https://programmers.co.kr/learn/courses/30/lessons/76502
-#프로그래머스-괄호 회전하기/풀이 참고함
+#프로그래머스-괄호 회전하기
 
 def solution(s):
-    answer = -1
-    
-    def check(s):
+    answer = 0
+    dic = {')':'(',']':'[','}':'{'}
+    cnt = 0
+    while cnt<len(s):
         stack = []
-        for a in s:
-            if a in ('(','[','{'):
-                stack.append(a)
-            else:
-                if not stack:
-                    return 0
-
-                before = stack.pop()
-                if a==')' and before!='(':
-                    return 0
-                if a==']' and before!='[':
-                    return 0
-                if a=='}' and before!='{':
-                    return 0
-        if not len(stack):
-            return 1
-        else: return 0
-            
-    for i in range(len(s)):
-        if i==0:
-            answer=check(s)
-        else:
-            s = s[1:]+s[0:1]
-            answer+=check(s)
-             
+        check = True
+        for i in range(len(s)):
+            if s[i] in ('(','[','{') : stack.append(s[i])
+            elif stack and dic[s[i]]==stack[-1]: stack.pop()
+            elif not stack:
+                check = False
+                break
+        if check and not stack: answer+=1
+        cnt+=1
+        s= s[1:]+s[0]
     return answer
