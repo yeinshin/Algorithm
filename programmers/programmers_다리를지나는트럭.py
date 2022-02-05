f33f953de29586e912cabdd9f1c5f07459dfd7ea
yein <syi05111@naver.com>
2022-02-05 12:21:09 +0900

    프로그래머스-다리를 지나는 트럭

--- programmers/programmers_다리를지나는트럭.py
+++ programmers/programmers_다리를지나는트럭.py
@@ -1,20 +1,20 @@
 #https://programmers.co.kr/learn/courses/30/lessons/42583
-#프로그래머스-다리를 지나는 트럭/풀이 참고함 ㅜ 
+#프로그래머스-다리를 지나는 트럭
 
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-    bridge = deque([0]*bridge_length)
-
-    while bridge:
-        bridge.popleft()
+    bridge_truck = deque([0]*bridge_length)
+    sum_weight = 0
+    
+    while bridge_truck:
         answer+=1
-
+        sum_weight-=bridge_truck.popleft()
         if truck_weights:
-            if sum(bridge)+truck_weights[0]<=weight:
-                bridge.append(truck_weights.pop(0))
-            else:
-                bridge.append(0)
-
+            if sum_weight+truck_weights[0]<=weight: 
+                bridge_truck.append(truck_weights.pop(0))
+                sum_weight+=bridge_truck[-1]
+            else: bridge_truck.append(0)
+        
     return answer
