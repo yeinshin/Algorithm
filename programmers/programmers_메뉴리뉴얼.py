73899953cf722a55459ff171c8cd993f724cd087
yein <syi05111@naver.com>
2022-02-13 14:29:04 +0900

    코드 수정

--- programmers/programmers_메뉴리뉴얼.py
+++ programmers/programmers_메뉴리뉴얼.py
@@ -1,31 +1,17 @@
 #https://programmers.co.kr/learn/courses/30/lessons/72411
 #프로그래머스-메뉴 리뉴얼
 from itertools import combinations
-from collections import defaultdict
+from collections import Counter
 
 def solution(orders, course):
     answer = []
     for c in course:
-        menu = list(combinations(sorted(list(set(''.join(orders)))),c))
-        dict = defaultdict(list)
-        for m in menu:
-            cnt=0
-            for i in range(len(orders)):
-                check = False
-                for a in m:
-                    if a not in orders[i]:
-                        check =False
-                        break
-                    else:
-                        check = True
-                if check:
-                    cnt+=1
-            if cnt >= 2:
-                dict[cnt].append(''.join(m))
-        if len(dict)!=0:
-            for d in dict[max(dict)]:
-                answer.append(d)
-        else:
-            break
-                
+        temp = []
+        for order in orders:
+            combi = combinations(sorted(order), c)
+            temp += combi
+        counter = Counter(temp)
+        if len(counter) != 0 and max(counter.values()) != 1:
+            answer += [''.join(f) for f in counter if counter[f] == max(counter.values())]
+
     return sorted(answer)
