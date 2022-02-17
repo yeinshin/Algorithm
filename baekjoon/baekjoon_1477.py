6be3b8214a81ed4b9d0aea08c56696ed7b7543f8
yein <syi05111@naver.com>
2022-02-17 19:08:03 +0900

    코드 수정

--- baekjoon/baekjoon_1477.py
+++ baekjoon/baekjoon_1477.py
@@ -1,19 +1,18 @@
 #https://www.acmicpc.net/problem/1477
 #1477번-휴게소 세우기
 import heapq
-from statistics import harmonic_mean
+import sys
+input = sys.stdin.readline
 n,m,l = map(int,input().split())
 arr = [0]+sorted(map(int,input().split()))+[l]
 d = []
-for i in range(n+1):
-    a = arr[i+1]-arr[i]
-    heapq.heappush(d,(-a,1))
+for i in range(n+1): heapq.heappush(d,(-(arr[i+1]-arr[i]),1))
 while m>0:
     a,div = heapq.heappop(d)
     og = abs(a*div)
     half =og/(div+1)
-    if og%(div+1)!=0: heapq.heappush(d,(-(int(half)+1),div+1))
-    else: heapq.heappush(d,(-int(half),div+1))
+    heapq.heappush(d,(-half,div+1))
     m-=1
-
-print(abs(heapq.heappop(d)[0]))
+ans = abs(heapq.heappop(d)[0])
+t = int(ans)
+print(t+1 if ans!=t else t)
