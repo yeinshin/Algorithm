2d624a92b29c951b8071824a09bec909baa9166e
yein <syi05111@naver.com>
2022-02-27 03:04:13 +0900

    코드 수정

--- baekjoon/baekjoon_7569.py
+++ baekjoon/baekjoon_7569.py
@@ -3,13 +3,16 @@
 
 # 상자의 크기를 나타내는 두 정수 M,N과 쌓아올려지는 상자의 수를 나타내는 H
 from collections import deque
+import sys
+input = sys.stdin.readline
+
 m,n,h = map(int,input().split())
 arr = [[[0]*h for _ in range(m)] for _ in range(n)]
-visited = [[[0]*h for _ in range(m)] for _ in range(n)]
 dx,dy,dz = [1,-1,0,0,0,0],[0,0,1,-1,0,0],[0,0,0,0,1,-1]
 q = deque()
 check_1 = 0
 check_0 = 0
+
 for k in range(h):
     for i in range(n):
         a = list(map(int,input().split()))
@@ -17,7 +20,6 @@ for k in range(h):
             arr[i][j][k] = a[j]
             if(a[j]==1): 
                 q.append((i,j,k,0))
-                visited[i][j][k]=1
                 check_1+=1
             elif a[j]==-1: check_0+=1
 ans = 0
@@ -30,11 +32,12 @@ def bfs():
             nx = dx[i] + x
             ny = dy[i] + y
             nz = dz[i] + z
-            if 0<=nx<n and 0<=ny<m and 0<=nz<h and visited[nx][ny][nz]==0 and arr[nx][ny][nz]==0:
-                visited[nx][ny][nz] = 1
+            if 0<=nx<n and 0<=ny<m and 0<=nz<h and arr[nx][ny][nz]==0:
+                arr[nx][ny][nz] = 1
                 q.append((nx,ny,nz,cnt+1))
                 check_1+=1
     return  ans
 
 bfs()
-print(ans if check_1==(n*m*h-check_0) else -1)
+if check_1==(n*m*h-check_0): print(ans)
+else: print(-1)
